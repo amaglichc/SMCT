be45fc5497dd57c3a8d2038e88e1e4799cca6339
Michael Istomin <amaglichc@gmail.com>
2024-12-08 16:38:05 +0500

    add basic structure

---
+++ backend/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return "Hello world!"
+
+if __name__ == "__main__":
+    uvicorn.run("main:app",port=8080)
